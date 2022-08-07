2f6d9535dde88108c9885051f67dc32919997c6c
ViktorSivashev <viktor.sivashev@mail.ru>
2022-08-07 17:22:34 +0300

    task27

--- Task27/Program.cs
+++ Task27/Program.cs
@@ -3,4 +3,15 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.WriteLine("Введите число: ");
+int numder = Convert.ToInt32(Console.ReadLine());
 
+int sum = 0;
+
+while (numder > 0)
+{
+    int num = numder % 10;
+    numder = numder / 10;
+    sum = num + sum;
+}
+Console.WriteLine($"Ответ - {sum}");
